3c17e3f825558ace3b91925227b01a8570d58e3e
marutimuthu <marutimuthu@gmail.com>
2022-03-09 18:55:14 +0530

    Initial Commit

---
+++ examples/simple.js
@@ -0,0 +1,24 @@
+var parseString = require('xml2js').parseString;
+// var xml = "<root>Hello xml2js!</root>"
+var xml = '<?xml version=”1.0" encoding=”UTF-8"?>' +
+'<Student>' +
+	'<PersonalInformation>' +
+		'<FirstName>Sravan</FirstName>' +
+		'<LastName>Kumar</LastName>' +
+		'<Gender>Male</Gender>' +
+	'</PersonalInformation>' +
+	'<PersonalInformation>' +
+		'<FirstName>Sudheer</FirstName>' +
+		'<LastName>Bandlamudi</LastName>' +
+		'<Gender>Male</Gender>' +
+	'</PersonalInformation>' +
+'</Student>';
+
+parseString(xml, function (err, result) {
+    // console.dir(result);
+
+    let data = result.Student.PersonalInformation;
+    console.dir(data[0].FirstName);
+
+    // let data = JSON.stringify(result)
+});
